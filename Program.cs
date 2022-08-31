ffb127d35e439fde621d5f5511ab6bea28534ece
10Strel <manowartenarrows@gmail.com>
2022-08-31 19:57:05 +0300

    final commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Напишите программу, которая на вход принимает два целых числа\r\nи выдаёт, какое число большее, а какое меньшее.\r\n");
+
+Console.WriteLine("Введите первое число:");
+
+bool res = int.TryParse(Console.ReadLine(), out int firstNumber);
+if (!res)
+{
+    Console.WriteLine("Ошибка ввода.\r\nВыполнение программы будет остановлено.");
+    return;
+}
+
+Console.WriteLine("Введите второе число:");
+
+res = int.TryParse(Console.ReadLine(), out int secondNumber);
+if (!res)
+{
+    Console.WriteLine("Ошибка ввода.\r\nВыполнение программы будет остановлено.");
+    return;
+}
+
+Console.WriteLine($"Максиальное введенное число: {(firstNumber > secondNumber ? firstNumber : secondNumber)}");
